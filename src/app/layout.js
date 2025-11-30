062b837ae212de8a1b6e25c2cc2290bbe2b41c85
l15440330-ship-it <l15440330@gmail.com>
2025-11-30 10:36:28 +0300

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+import { ThemeProvider } from "../contexts/ThemeContext";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "AgenticSeek",
+  description: "AgenticSeek Next.js Migration",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
